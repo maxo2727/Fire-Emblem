4dc2883555695d40ef0981aa89621c13a227366a
maxo2727 <maximiliano.valdivia27@gmail.com>
2024-10-14 23:24:20 -0300

    Players Dict Fix

--- Fire-Emblem/PlayersFolder/Players.cs
+++ Fire-Emblem/PlayersFolder/Players.cs
@@ -2,16 +2,11 @@ namespace Fire_Emblem.PlayersFolder;
 
 public class Players
 {
-    public Dictionary<int, Player> PlayersDict;
-
-    public Players()
+    public Dictionary<int, Player> PlayersDict = new Dictionary<int, Player>()
     {
-        Player player1 = new Player();
-        Player player2 = new Player();
-        PlayersDict = new Dictionary<int, Player>();
-        PlayersDict.Add(1, player1);
-        PlayersDict.Add(2, player2);
-    }
+        {1, new Player()},
+        {2, new Player()},
+    };
 }
 
 // almacenar flujo de turnos aca por ej
